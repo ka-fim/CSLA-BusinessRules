8c83b508f9d9746a1d4cf83a7a390df99119bbc4
Matthias Fischer <matthias.fischer@ni.com>
2024-02-16 14:13:37 +0100

    Add project files.

---
+++ CSLA-BusinessRules/BusinessClass.cs
@@ -0,0 +1,42 @@
+﻿using Csla;
+using Csla.Rules;
+using Csla.Rules.CommonRules;
+
+[Serializable]
+public class BusinessClass : BusinessBase<BusinessClass>
+{
+    public static readonly PropertyInfo<string> PrimaryProperty = RegisterProperty<string>(c => c.Primary);
+
+    public static readonly PropertyInfo<string> AdditionalInputProperty = RegisterProperty<string>(c => c.AdditionalInput);
+
+    public string Primary
+    {
+        get => GetProperty(PrimaryProperty);
+        set => SetProperty(PrimaryProperty, value);
+    }
+
+    public string AdditionalInput
+    {
+        get => GetProperty(AdditionalInputProperty);
+        set => LoadProperty(AdditionalInputProperty, value);
+    }
+
+    protected override void AddBusinessRules()
+    {
+        BusinessRules.AddRule(new Lambda(CustomRule)
+        {
+            PrimaryProperty = PrimaryProperty,
+            InputProperties = { AdditionalInputProperty }
+        });
+    }
+
+    private void CustomRule(IRuleContext context)
+    {
+        Console.WriteLine("Rule is executed");
+    }
+
+    [Create]
+    private void Create()
+    {
+    }
+}
---
+++ CSLA-BusinessRules/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Csla;
+using Csla.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+services.AddCsla();
+var provider = services.BuildServiceProvider();
+var dp = provider.GetRequiredService<IDataPortal<BusinessClass>>();
+
+var bo = dp.Create();
+
+Console.WriteLine("Setting primary property");
+bo.Primary = "test";
+Console.WriteLine("Setting additional input property");
+bo.AdditionalInput = "test2";
